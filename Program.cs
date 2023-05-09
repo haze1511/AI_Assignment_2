995be0cc17be49c2b4caba17ca5bba5c5a634e5c
haze1511 <88806682+haze1511@users.noreply.github.com>
2023-05-09 13:55:50 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Interference_Engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser parser = new Parser(args[1]); // providing file to parser
+            
+            // for debugging
+            Console.WriteLine("ASK: " + parser.Query);
+            Console.WriteLine("TELL: " + parser.Inform);
+
+            KnowledgeBase KB; 
+           
+            switch (args[0].ToLower()) // based on method 
+            {
+                case "bc": //backward chaining
+                    KB = new BackwardChaining(parser.Query, parser.Inform);
+                    break;
+
+                case "fc": //forward chaining
+                    KB = new ForwardChaining(parser.Query, parser.Inform);
+                    break;
+
+               // case "tt": // truth table 
+               //     KB = new TruthTable(parser.Query, parser.Inform);
+               //     break;     
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
